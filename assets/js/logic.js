374e17617a529e755fb2caed6307e2ac79d5a38e
blackclover221 <yasminismail33@gmail.com>
2023-11-03 18:50:52 +0000

    changed the folder structure and added question to script file and started functions on the logic side of the quiz

---
+++ assets/js/logic.js
@@ -0,0 +1,53 @@
+// variables to keep track of the state 
+
+let currentQuestionIndex = 0;
+let time = questions.length * 15;
+let timerID;
+
+// HTML elements; 
+
+let questionElement = document.getElementById("questions");
+let timerElement = document.getElementById("time");
+let choicesElement = document.getElementById("choices")
+let submitButton = document.getElementById("submit");
+let startButton = document.getElementById("start");
+let initialElement = document.getElementById("initals");
+let feedBackElement = document.getElementById("feedback");
+
+
+let sfxRight = new Audio("assests/sfx/correct.wav")
+
+function startQuiz() {
+    alert("Hi")
+
+}
+
+function questionClick() {
+
+}
+
+function getQuestion() {
+
+}
+
+function quizEnd() {
+
+
+}
+
+function clockTick() {
+
+}
+
+function saveHighScore() {
+
+}
+
+function checkForEnter(event) {
+
+}
+
+startButton.addEventListener("click", startQuiz);
+
+submitButton.addEventListener("submit")
+
